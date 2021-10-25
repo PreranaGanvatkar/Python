937164af5672c20c69a13adb790dc8c9acb7c230
PreranaGanvatkar <preranaganvatkar@gmail.com>
2021-10-25 21:46:03 +0530

    code is modified

---
+++ add2number.py
@@ -0,0 +1,6 @@
+print("enter the number1")
+num1 = int(input())
+print("enter the number2")
+num2 = int(input())
+sum=num1+num2;
+print("the sum of "+str(num1)+" and "+str(num2)+" = "+str(sum))
---
+++ vardeclare.py
@@ -0,0 +1,7 @@
+person_name="xyz"
+object_1="car"
+object_1_color="red"
+object_2="house"
+print("Hello ,I'm "+person_name)
+print("I'm interested in your "+object_1+" which is "+object_1_color+" in color")
+print("and your "+object_2+" is also beautiful")
