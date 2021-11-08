d2f3909bf1f1cb9ddc45ef36c711f8ddfb7b523e
PreranaGanvatkar <preranaganvatkar@gmail.com>
2021-11-08 22:18:23 +0530

    update

---
+++ exapmle/mathoperator.py
@@ -0,0 +1,14 @@
+# basics of python
+print(2+2)
+
+print((2 + 3) * 6)
+
+print(23 / 7)
+
+print(23 // 7)
+
+print((5 - 1) * ((7 + 1) / (3 - 1)))
+
+print(23 % 7)
+
+#error print(42 + 5 + * 2)
---
+++ exapmle/stringconcatination.py
@@ -0,0 +1,9 @@
+print('Alice' + 'Bob')
+
+#type error print('Alice' + 42)
+
+print('Alice' * 5)
+
+#type error print('Alice' * 'Bob')
+
+#type erroe print('Alice' * 5.0)
