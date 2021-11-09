69130f4587fe57c5db7877de85ff085f86a6c445
PreranaGanvatkar <preranaganvatkar@gmail.com>
2021-11-09 20:24:14 +0530

    update

---
+++ exapmle/decalringvariables.py
@@ -0,0 +1,9 @@
+#declaring the variable
+
+x,y,z="orange","banana","cherry"
+print(x)
+print(y)
+print(z)
+
+x=y=z
+print(z)
---
+++ exapmle/ifstatement.py
@@ -0,0 +1,12 @@
+#using if condition
+
+print("Enter 1st number:")
+num1=input()
+print("Enter 2nd number")
+num2=input()
+if num1==num2:
+    print("1st number is equal to 2nd number")
+elif num1>num2:
+    print("1st number is greater then 2nd number")
+else:
+    print("2nd number is greater then 1st number")
